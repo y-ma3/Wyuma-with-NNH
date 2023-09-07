a73f52fb00339bbcb4e6bce28995bfdb8fdad46c
4na7i1 <4na7i1@gmail.com>
2023-09-07 14:53:52 +0900

    test user-name,user-icon

--- test-nakanoue/src/App.js
+++ test-nakanoue/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="flex">
+        <img src={humanPng} alt="picture" />
+        <p className="user-name">TEST USER01</p>
+      </div>
       <p class="account-number">口座番号：10000000</p>
     </div>
   );
