599434f04864c9aeb4fb81f21a89e1b2431176f0
kilzeast <kil0zeast@gmail.com>
2023-09-08 13:20:45 +0900

    higashi

--- test-nakanoue/src/pages/MoneyTransfer.js
+++ test-nakanoue/src/pages/MoneyTransfer.js
@@ -21,22 +21,32 @@ function MoneyTransfer() {
     const handleAmountChange = (event) => {
         const amount = event.target.value;
         setTransferAmount(amount);
-        setErrorMessage(''); // 送金額が変更されたらエラーメッセージをクリア
+
+        if (amount === '' || parseFloat(amount) < 0 || parseFloat(amount) > balance) {
+            setErrorMessage(`送金額は0以上かつ${balance}以下である必要があります。`);
+        } else {
+            setErrorMessage('');
+        }
     }
 
     // 送金額が空の場合にボタンを無効にする条件
     const isButtonDisabled = transferAmount === '' || parseFloat(transferAmount) < 0 || parseFloat(transferAmount) > balance;
 
+    // 親要素にスタイルを適用するスタイルオブジェクト
+    const paragraphStyle = {
+        fontSize: '24px',
+    };
+
     return (
         <div className="App">
-            <p>Money Transfer</p>
+            <p style={paragraphStyle}>Money Transfer</p>
             <div className="recipient-info">
-                <p>送金先</p>
-                <p>{userName}</p>
-                <p>送金上限額: {balance} 円</p>
+                <p style={paragraphStyle}>送金先</p>
+                <p style={paragraphStyle}>{userName}</p>
+                <p style={paragraphStyle}>送金上限額: {balance} 円</p>
             </div>
             <div className="amount-input">
-                <label htmlFor="transfer-amount">送金額: </label>
+                <label style={paragraphStyle} htmlFor="transfer-amount">送金額: </label>
                 <input
                     type="number"
                     id="transfer-amount"
@@ -46,8 +56,8 @@ function MoneyTransfer() {
                     max={balance}
                 />
             </div>
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-            <button onClick={handleSubmit} disabled={isButtonDisabled}>送金</button>
+            {errorMessage && <p style={{ color: 'red', fontSize: '18px' }}>{errorMessage}</p>}
+            <button style={paragraphStyle} onClick={handleSubmit} disabled={isButtonDisabled}>送金</button>
         </div>
     );
 }
