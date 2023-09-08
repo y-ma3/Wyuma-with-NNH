bb7b84b85f01cbe201203d985561dc1bf2822eff
4na7i1 <4na7i1@gmail.com>
2023-09-08 13:42:00 +0900

    minor revision design

--- test-nakanoue/src/pages/TransferSuccess.js
+++ test-nakanoue/src/pages/TransferSuccess.js
@@ -3,9 +3,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import '../App.css';
-import humanPng2 from '../images/human2.png';
-import humanPng3 from '../images/human3.png';
-import humanPng4 from '../images/human4.png';
+import approvalPng from '../images/approval.png';
 
 function SendTest() {
     const navigate = useNavigate();
@@ -17,6 +15,14 @@ function SendTest() {
     return (
         <div className="App">
             <div className="header">
+                <div style={{ paddingTop: '50px' }}>
+                    <img
+                        className="approval-icon"
+                        src={approvalPng}
+                        alt="picture"
+                        style={{ width: '20%' }}
+                    />
+                </div>
                 <p>送金処理が成功しました</p>
             </div>
             <button onClick={handleSubmit}>トップ画面に戻る</button>
